e144f7fa037724881225ad8b9082aa42ec161629
Karina Lucindo <karina.lucindo@outlook.com>
2022-06-23 16:27:39 -0300

    Components created

---
+++ src/components/Video.tsx
@@ -0,0 +1,8 @@
+export function Video() {
+  return (
+    // flex-1: Elemento vai poder esticar ou reduzir conforme precise de mais ou menos tamanho
+    <div className="flex-1">
+      Video
+    </div>
+  )
+}
