d9b844b09f72d05f29ac57f3cb7a4d7dc31a3c83
Karina Lucindo <karina.lucindo@outlook.com>
2022-06-24 16:57:24 -0300

    New page added

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -39,7 +39,8 @@ export function Video(props: VideoProps) {
   const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
     variables: {
       slug: props.lessonSlug
-    }
+    },
+    fetchPolicy: 'no-cache'
   }) 
 
   if (!data) {
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4otsktg10y001z4e4ni88ww/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
